83fa88d2339fa012d0bb95d98e4a2016ce55ab9c
Paulo Canuto <paulohfcanuto@gmail.com>
2019-08-01 23:54:26 -0300

    32. Component Nesting

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import faker from 'faker';
+
+const CommentDetail = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Hoje as 18:00 horas</span>
+          </div>
+          <div className="text">I want do BLOOG for them!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReacDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date"> Hoje as 18:00 horas</span>
-          </div>
-          <div className="text">I want do BLOOG for them!</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };
