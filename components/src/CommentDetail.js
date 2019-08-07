3bb5cf8db12736bd958b8924a09ce637e4f350f7
Paulo Canuto <paulohfcanuto@gmail.com>
2019-08-07 00:00:06 -0300

    36. Passing Props - Solutions

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = props => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" src={props.avatar} />
         </a>
         <div className="content">
           <a href="/" className="author">
             {props.author}
           </a>
           <div className="metadata">
-            <span className="date"> Hoje as 18:00 horas</span>
+            <span className="date">{props.timeAgo}</span>
           </div>
           <div className="text">I want do BLOOG for them!</div>
         </div>
--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReacDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Pedro" />
-      <CommentDetail author="Paulo" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Fernando" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Hoje as 18:00 horas"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Pedro"
+        timeAgo="Hoje as 11:20 horas"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Paulo"
+        timeAgo="Hoje as 18:04 horas"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Hoje as 18:45 horas"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Fernando"
+        timeAgo="Hoje as 08:12 horas"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
