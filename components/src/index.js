fef9f00ca9f59b4358e41d50f90367ed0cc13c99
Paulo Canuto <paulohfcanuto@gmail.com>
2019-08-11 23:55:02 -0300

    40. Component Reuse

--- components/src/index.js
+++ components/src/index.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import ReacDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Hoje as 18:00 horas"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Pedro"
-        timeAgo="Hoje as 11:20 horas"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Paulo"
-        timeAgo="Hoje as 18:04 horas"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Hoje as 18:45 horas"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Fernando"
-        timeAgo="Hoje as 08:12 horas"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Hoje as 18:00 horas"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <div>
+          <h4>Vai dar treta!</h4>
+          Tem certeza que quer fazer isso?
+        </div>
+      </ApprovalCard>
     </div>
   );
 };
